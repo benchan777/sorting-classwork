74aac2f8ff8911c0a2fbdbe1ee9a91db6b62a2c7
benchan777 <benchan777@gmail.com>
2022-04-04 14:34:14 -0700

    finished quick sort

--- sorting_recursive.py
+++ sorting_recursive.py
@@ -72,33 +72,80 @@ def merge_sort(items):
     return items
 
 def partition(items, low, high):
-    """Return index `p` after in-place partitioning given items in range
+    """
+    Return index `p` after in-place partitioning given items in range
     `[low...high]` by choosing a pivot (TODO: document your method here) from
     that range, moving pivot into index `p`, items less than pivot into range
     `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
+
+    For simplicity, I have chosen the pivot point to be the last number within 
+    the input array.
+
     TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+
+    The runtime is O(n) because we will always need to iterate through the entire 
+    array and individually compare each and every number within the array to the 
+    pivot point to determine if swapping is necessary.
+
+    TODO: Memory usage: ??? Why and under what conditions?
+
+    The memory usage is O(1) because all of the swapping being done is done in 
+    place so no copy of the input array is ever made and the memory usage stays 
+    the same no matter the size of the input array.
+    """
     # TODO: Choose a pivot any way and document your method in docstring above
     # TODO: Loop through all items in range [low...high]
     # TODO: Move items less than pivot into front of range [low...p-1]
     # TODO: Move items greater than pivot into back of range [p+1...high]
     # TODO: Move pivot item into final position [p] and return index p
+    pointer = low #the pointer is a number larger than the pivot
+    
+    for i in range(low, high): #iterate through array from specified low to high index
+        if items[i] <= items[high]: #compare each number against the pivot (last number)
+            items[i], items[pointer] = items[pointer], items[i] #if number is smaller than pivot, move it to the left of pointer
+            pointer += 1
+    
+    #swap pivot with pointer so all numbers less than pivot are to the left and all numbers larger are to the right
+    items[high], items[pointer] = items[pointer], items[high] 
+    return pointer 
 
 
 def quick_sort(items, low=None, high=None):
-    """Sort given items in place by partitioning items in range `[low...high]`
+    """
+    Sort given items in place by partitioning items in range `[low...high]`
     around a pivot item and recursively sorting each remaining sublist range.
+
     TODO: Best case running time: ??? Why and under what conditions?
+
+    The best case time complexity is O(nlogn). This occurs when the partitions 
+    being passed into the function recursively are evenly balanced and thus, 
+    the amount of operations being done every time is halved.
+
     TODO: Worst case running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+
+    The worst case time complexity is O(n^2). This occurs when the partitions 
+    being passed into the function recursively are not even balanced which causes 
+    the algorithm to need to be run n*n number of times.
+
+    TODO: Memory usage: ??? Why and under what conditions?
+
+    The memory usage is O(logn) where the memory being used is by the call stack 
+    each time quick sort is called.
+    """
     # TODO: Check if high and low range bounds have default values (not given)
     # TODO: Check if list or range is so small it's already sorted (base case)
     # TODO: Partition items in-place around a pivot and get index of pivot
     # TODO: Sort each sublist range by recursively calling quick sort
+    if not low or not high:
+        low, high = 0, len(items) - 1
 
-# arr1 = [1,3,5,6]
-# arr2 = [2,3,4,7,8]
-# arr3 = [4,67,2,4,6,8,4,2,56,8,534,5,45]
-# print(arr3)
-# merge_sort(arr3)
-# print(arr3)
+    if low < high:
+        pivotIndex = partition(items, low, high)
+
+        quick_sort(items, low, pivotIndex - 1)
+        quick_sort(items, pivotIndex + 1, high)
+
+array = [8,7,6,1,0,9,2]
+print(array)
+quick_sort(array, 0, 6)
+print(array)
